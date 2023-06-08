91efb0def51606fae0700ab21919da5aa6d81252
KryptoGhost <106913123+KryptoGhost@users.noreply.github.com>
2023-06-08 21:33:33 +0100

    COMMIT

---
+++ app.js
@@ -0,0 +1,48 @@
+let year = document.querySelector('.year');
+let month= document.querySelector('.month');
+let days = document.querySelector('.day');
+let error = document.querySelector('.error');
+const submit  = document.querySelector('.submit');
+const yearage = document.querySelector('.span-year');
+const monthage = document.querySelector('.span-month');
+const daysage = document.querySelector('.span-days');
+
+
+
+submit.addEventListener('click', function () {
+    year = parseInt(year.value);
+    month = parseInt (month.value);
+    days = parseInt(days.value);
+
+    let dob = new Date(year, month, days);
+    let today = new Date();
+
+    if (isNaN(dob) || dob > today || month < 0 || month > 11) {
+        error.innerHTML = 'invalid date of birth';
+        return;
+        //clearIt();
+    }
+    
+    let isValid = (dob.getFullYear() === year && dob.getMonth() === month && dob.getDate() === days);
+    if (!isValid) {
+        error.innerHTML = 'invalid date of birth';
+        return;
+       // clearIt();
+    }
+
+    let ageYear = today.getFullYear() - dob.getFullYear();
+    let ageMonth = today.getMonth() - dob.getMonth();
+    let ageDays = today.getDate() - dob.getDate();
+
+    yearage.textContent = ageYear;
+    monthage.textContent = ageMonth;
+    daysage.textContent = ageDays;
+
+    clearIt();
+});
+
+function clearIt() {
+    year.value = "";
+    month.value = "";
+    days.value = "";
+}
